f630324db79e734aea8287af70f1f13530dc12fd
TaeHyeon Kim <kth990303@naver.com>
2021-08-25 22:15:35 +0900

    TDD Test코드 작성 완료

--- src/test/java/algopa/study/member/MemberServiceImplTest.java
+++ src/test/java/algopa/study/member/MemberServiceImplTest.java
@@ -9,11 +9,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
+@Rollback
 class MemberServiceImplTest {
     @Autowired
     MemberService service;
@@ -30,15 +33,25 @@ class MemberServiceImplTest {
 
     @Test
     void edit() {
+        Member member=new Member("test", "Silver V");
+        Member member2=new Member("test", "Silver IV");
+        // 현재 테스트 대상은 티어만 바꾸는 member1, member2만
+        Member member3=new Member("test2", "Silver V");
+        Member member4=new Member("test2", "Silver IV");
+        repository.save(member);
+        Optional<Member> findMemberOptional = repository.findByName(member.getName());
+        Member findMember = findMemberOptional.get();
+
+        service.edit(findMember.getId(), member2);
+
+        Assertions.assertThat(findMember.getTier()).isEqualTo("Silver IV");
     }
 
     @Test
-    @Transactional
-    @Rollback
     void join() {
         Member member=new Member("test", "Silver V");
 
-        repository.save(member);
+        service.join(member);
         Optional<Member> findMemberOptional = repository.findByName(member.getName());
         Member findMember = findMemberOptional.get();
 
@@ -47,13 +60,45 @@ class MemberServiceImplTest {
 
     @Test
     void deleteMember() {
+        Member member=new Member("test", "Silver V");
+
+        repository.save(member);
+        List<Member> memberList = repository.findAll();
+        service.deleteMember(member.getId());
+        List<Member> members = repository.findAll();
+
+        Assertions.assertThat(members.size()).isEqualTo(memberList.size()-1);
     }
 
     @Test
     void checkDuplicateMember() {
+        Member member=new Member("test", "Silver V");
+        Member member2=new Member("test", "Silver V");
+        Member member3=new Member("test", "Platinum II");
+
+        repository.save(member);
+        Boolean canMember2 = service.checkDuplicateMember(member2);
+        Boolean canMember3 = service.checkDuplicateMember(member3);
+
+        Assertions.assertThat(!canMember2 && !canMember3).isEqualTo(true);
     }
 
     @Test
     void findAllMembers() {
+        Member member=new Member("test", "Silver V");
+        Member member2=new Member("test", "Silver IV");
+        Member member3=new Member("test2", "Silver V");
+        Member member4=new Member("test2", "Silver IV");
+
+        repository.save(member);
+        repository.save(member2);
+        repository.save(member3);
+        repository.save(member4);
+        List<Member> allMembers = service.findAllMembers();
+
+        Assertions.assertThat(allMembers).contains(member);
+        Assertions.assertThat(allMembers).contains(member2);
+        Assertions.assertThat(allMembers).contains(member3);
+        Assertions.assertThat(allMembers).contains(member4);
     }
 }
